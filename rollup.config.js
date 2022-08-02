6d3e871dd85c23b70a351597e65959f6c56ed7ad
Kerry Su <me@sshockwave.net>
2022-08-02 12:18:16 +0800

    Feat add rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+// rollup.config.js
+
+import resolve from '@rollup/plugin-node-resolve'
+
+import { chromeExtension, simpleReloader } from 'rollup-plugin-chrome-extension'
+
+export default {
+  input: 'src/manifest.json',
+  output: {
+    dir: 'dist',
+    format: 'esm',
+  },
+  plugins: [
+    // always put chromeExtension() before other plugins
+    chromeExtension(),
+    simpleReloader(),
+    // the plugins below are optional
+    resolve(),
+  ],
+}
