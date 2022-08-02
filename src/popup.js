e541aa52ffa5b3a41cb8c8cfa13d355bb4bc8ebf
Kerry Su <me@sshockwave.net>
2022-08-02 17:04:47 +0800

    Feat(popup) add material ui

--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,34 @@
 import { createRoot } from 'react-dom/client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Autocomplete, Box, TextField } from '@mui/material';
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  const [options, setOptions] = useState([]);
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    if (!ready) {
+      chrome.storage.sync.get(['search_engines'], ({ search_engines }) => {
+        search_engines ||= {};
+        setOptions(Object.keys(search_engines));
+        setReady(true);
+      });
+    }
+  });
+  return <Box component='form'>
+    <Autocomplete
+      open
+      options={options}
+      renderInput={(params) => <TextField
+          label="Outlined"
+          variant="outlined"
+          sx={{ minWidth: '20rem' }}
+          margin="dense"
+          autoFocus
+          fullWidth
+          {...params}
+      />}
+      />
+  </Box>;
 }
 
 createRoot(document.getElementById('root')).render(<App/>);
